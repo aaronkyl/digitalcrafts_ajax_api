03ad64ab1f94ec521d12fa6911f024c231331a68
Aaron Wilkinson <aaronwilkinson@gmail.com>
2018-06-06 00:33:21 +0000

    server serves API

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const nunjucks = require('nunjucks');
+const axios = require('axios');
+require('dotenv').load();
 
 var app = express();
 
@@ -15,6 +17,21 @@ app.get('/', function(req, resp) {
     resp.render('index.html', {});
 });
 
+app.get('/search', function(req, res) {
+    var userInput = req.query.city;
+    var api_url = 'http://api.openweathermap.org/data/2.5/weather';
+    var config = {
+        params: {
+            q: userInput,
+            APPID: process.env.APPID
+        }
+    };
+    axios.get(api_url, config)
+    .then(function(response) {
+        res.json(response.data);
+    });
+});
+
 app.listen(8080, function() {
-    console.log("Listening on port 8080");
+    console.log('Listening on port 8080');
 });
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,16 +2,15 @@ $(function() {
     $('button').click(function() {
         var userInput = $('input').val();
         if (userInput) {
-            var api_url = 'http://api.openweathermap.org/data/2.5/weather';
+            var api_url = '/search';
             var config = {
               params: {
-                q: userInput,
-                APPID: 'API KEY HERE'
+                city: userInput
               }
             };
             axios.get(api_url, config)
             .then(function(response) {
-                $('#APIresults').text(JSON.stringify(response.data));
+                $('#APIresults').text(JSON.stringify(response));
             });
         }
     });
